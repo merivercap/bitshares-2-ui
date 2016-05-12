fe94e2a4e0bfae9e5fdc8339f835cdc2d4802ea0
merivercap <jun@peerhub.com>
2016-05-12 16:48:11 -0700

    change dashboard ui

--- web/app/components/Dashboard/Dashboard.jsx
+++ web/app/components/Dashboard/Dashboard.jsx
@@ -72,59 +72,11 @@ class Dashboard extends React.Component {
 
         let names = this.props.linkedAccounts.toArray().sort();
         let ignored = this.props.myIgnoredAccounts.toArray().sort();
+
         
-        let featuredMarkets = [
-            ["BTS", "OPEN.STEEM"],
-            ["OPEN.BTC", "MKR"],
-            ["OPEN.BTC", "OPEN.DGD"],
-            ["OPEN.BTC", "OPEN.ETH"],
-            ["OPEN.BTC", "OPEN.STEEM"],
-            ["BTS", "BTSR"],
-            ["BTS", "OBITS"],
-            ["BTS", "USD"],
-            ["BTS", "CNY"],
-            ["BTC", "BTS"],
-            ["BTS", "GOLD"],
-            ["BTS", "SILVER"],
-            // ["BTS", "EUR"]
-        ];
-
-        let newAssets = [
-            "OPEN.STEEM",
-            "MKR",
-            "OPEN.DGD",
-            "OPEN.ETH",
-            "BTSR"
-        ];
-
-        let markets = featuredMarkets.map((pair, index) => {
-
-            let className = "";
-            if (index > 3) {
-                className += "show-for-medium";
-            }
-            if (index > 8) {
-                className += " show-for-large";
-            }
-            
-            return (
-                <MarketCard
-                    key={pair[0] + "_" + pair[1]}
-                    new={newAssets.indexOf(pair[1]) !== -1}
-                    className={className}
-                    quote={pair[0]}
-                    base={pair[1]}
-                />
-            );
-        })
-
         return (
             <div ref="wrapper" className="grid-block page-layout vertical">
                 <div ref="container" className="grid-container" style={{paddingTop: 25}}>
-                    <Translate content="exchange.featured" component="h4" />
-                    <div className="grid-block small-up-1 medium-up-3 large-up-4 no-overflow">
-                        {markets}
-                    </div>
 
                     <div className="generic-bordered-box" style={{marginBottom: 5}}>
                         <div className="block-content-header" style={{marginBottom: 15}}>
@@ -132,7 +84,7 @@ class Dashboard extends React.Component {
                         </div>
                         <div className="box-content">
                             <DashboardList accounts={Immutable.List(names)} width={width} />
-                            {myIgnoredAccounts.size ? 
+                            {myIgnoredAccounts.size ?
                                 <table className="table table-hover" style={{fontSize: "0.85rem"}}>
                                     <tbody>
                                         <tr>
@@ -142,20 +94,11 @@ class Dashboard extends React.Component {
                                                 </div>
                                             </td>
                                         </tr>
-                                    </tbody>                                
+                                    </tbody>
                                 </table> : null}
                             {showIgnored ? <DashboardList compact accounts={Immutable.List(ignored)} width={width} /> : null}
                         </div>
                     </div>
-
-                    <RecentTransactions
-                        style={{marginBottom: 20, marginTop: 20}}
-                        accountsList={this.props.linkedAccounts}
-                        limit={10}
-                        compactView={false}
-                        fullHeight={true}
-                        showFilters={true}
-                    />
 
                 </div>
             </div>);
